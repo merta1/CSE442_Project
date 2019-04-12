c9dd60fb5c0f73c7af220a0d4ce5722d09328c0a
Anubhav <Anubhav@LAPTOP-8VNGMGJM>
2019-04-12 14:38:52 -0400

    SwitchHandlerClasses with functions added

---
+++ spark/src/main/java/edu/buffalo/cse442/handlers/SwitchHandler.java
@@ -0,0 +1,15 @@
+package edu.buffalo.cse442.handlers;
+
+public class SwitchHandler
+{
+    public int isUserpartofDebate()
+    {
+        //If user is part of debate return 1 else return 0;
+        return 0;
+    }
+    public int  returnUserDebate()
+    {
+        //Return if the User has agreed or disagreed to the debate so that the other side can be made read only on react side
+        return 0;
+    }
+}
